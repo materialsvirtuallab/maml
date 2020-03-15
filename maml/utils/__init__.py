328177d25a16e87cbdf0d4e4a794cce86766f66f
Chi Chen <chc273@eng.ucsd.edu>
2020-03-15 10:58:37 -0700

    add valueprofile

--- maml/utils/__init__.py
+++ maml/utils/__init__.py
@@ -1,17 +1,19 @@
 """
 Utilities package.
 """
-from ._data_selection import MonteCarloSampler  # noqa
 from ._general import (serialize_maml_object, deserialize_maml_object,  # noqa
                        load_pickle, to_pickle)  # noqa
 from ._data_conversion import pool_from, convert_docs  # noqa
+from ._value_profile import ValueProfile, ConstantValue, LinearProfile  # noqa
 
 __all__ = [
-    'MonteCarloSampler',
     'serialize_maml_object',
     'deserialize_maml_object',
     'load_pickle',
     'to_pickle',
     'pool_from',
-    'convert_docs'
+    'convert_docs',
+    'ValueProfile',
+    'ConstantValue',
+    'LinearProfile'
 ]
---
+++ maml/utils/tests/test_value_profile.py
@@ -0,0 +1,24 @@
+import unittest
+
+from maml.utils import ConstantValue, LinearProfile
+
+
+class TestValueProfile(unittest.TestCase):
+
+    def test_constant(self):
+        constant_profile = ConstantValue(0.1)
+        constant_profile.increment_step()
+        self.assertEqual(constant_profile.step, 1)
+        self.assertAlmostEqual(constant_profile.get_value(), 0.1)
+
+    def test_linearprofile(self):
+        linear = LinearProfile(value_start=100, value_end=1, max_steps=99)
+
+        self.assertAlmostEqual(linear.rate, -1)
+        for _ in range(99):
+            linear.increment_step()
+        self.assertAlmostEqual(linear.get_value(), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
