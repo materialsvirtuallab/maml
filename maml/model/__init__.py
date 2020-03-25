a1b969ccef6f1c5a5362299c341bbd743122dba9
Chi Chen <chc273@eng.ucsd.edu>
2020-03-25 16:03:28 -0700

    update model example

--- maml/model/__init__.py
+++ maml/model/__init__.py
@@ -1,6 +1,10 @@
 """MAML models"""
 from ._neural_network import MultiLayerPerceptron  # noqa
+from maml.base import ModelWithSklearn, ModelWithKeras  # noqa
+
 
 __all__ = [
-    'MultiLayerPerceptron'
+    'MultiLayerPerceptron',
+    'ModelWithSklearn',
+    'ModelWithKeras'
 ]
