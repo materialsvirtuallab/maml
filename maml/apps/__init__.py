12703776ed4f1ea0824aa14d07777f59eebef5bd
Chi Chen <chc273@eng.ucsd.edu>
2020-03-25 08:25:15 -0700

    lift lammps calcs to apps namespace

--- maml/apps/__init__.py
+++ maml/apps/__init__.py
@@ -4,10 +4,27 @@ MAML apps
 
 # potential energy surface
 from .pes import GAPotential, MTPotential, NNPotential, SNAPotential  # noqa
+from .pes import (  # noqa
+    LMPStaticCalculator, EnergyForceStress,  # noqa
+    SpectralNeighborAnalysis, ElasticConstant,  # noqa
+    LatticeConstant, NudgedElasticBand, DefectFormation,  # noqa
+    get_lmp_exe, set_lmp_exe  # noqa
+    )
+
 
 __all__ = [
+    'Potential',
     'GAPotential',
     'MTPotential',
     'NNPotential',
-    'SNAPotential'
+    'SNAPotential',
+    "LMPStaticCalculator",
+    "EnergyForceStress",
+    "SpectralNeighborAnalysis",
+    "ElasticConstant",
+    "LatticeConstant",
+    "NudgedElasticBand",
+    "DefectFormation",
+    "get_lmp_exe",
+    "set_lmp_exe"
 ]
