a72a32c854f4a508d3944da52022cd91fd2a033d
Chi Chen <chc273@eng.ucsd.edu>
2020-04-02 16:51:01 -0700

    add base splitter to __init__

--- maml/utils/__init__.py
+++ maml/utils/__init__.py
@@ -7,7 +7,7 @@ from ._data_conversion import pool_from, convert_docs  # noqa
 from ._value_profile import ValueProfile, ConstantValue, LinearProfile  # noqa
 from ._jit import njit  # noqa
 from ._tempfile import MultiScratchDir  # noqa
-from ._data_split import ShuffleSplitter  # noqa
+from ._data_split import DataSplitter, ShuffleSplitter  # noqa
 from ._lammps import (write_data_from_structure,  # noqa
                       check_structures_forces_stresses, stress_format_change,  # noqa
                       stress_matrix_to_list, stress_list_to_matrix,  # noqa
@@ -25,11 +25,12 @@ __all__ = [
     'LinearProfile',
     'njit',
     'MultiScratchDir',
-    "ShuffleSplitter",
-    "write_data_from_structure",
-    "check_structures_forces_stresses",
-    "stress_format_change",
-    "stress_list_to_matrix",
-    "stress_matrix_to_list",
-    "get_lammps_lattice_and_rotation"
+    'DataSplitter',
+    'ShuffleSplitter',
+    'write_data_from_structure',
+    'check_structures_forces_stresses',
+    'stress_format_change',
+    'stress_list_to_matrix',
+    'stress_matrix_to_list',
+    'get_lammps_lattice_and_rotation'
 ]
