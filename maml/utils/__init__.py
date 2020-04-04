5f6a61707161b811acb44940e2b012ae46ac2004
Chi Chen <chc273@eng.ucsd.edu>
2020-04-03 18:59:07 -0700

    remove useless utils

--- maml/utils/__init__.py
+++ maml/utils/__init__.py
@@ -1,8 +1,6 @@
 """
 Utilities package.
 """
-from ._general import (serialize_maml_object, deserialize_maml_object,  # noqa
-                       load_pickle, to_pickle)  # noqa
 from ._data_conversion import pool_from, convert_docs  # noqa
 from ._value_profile import ValueProfile, ConstantValue, LinearProfile  # noqa
 from ._jit import njit  # noqa
@@ -16,10 +14,6 @@ from ._lammps import (write_data_from_structure,  # noqa
 from ._stats import Stats  # noqa
 
 __all__ = [
-    'serialize_maml_object',
-    'deserialize_maml_object',
-    'load_pickle',
-    'to_pickle',
     'pool_from',
     'convert_docs',
     'ValueProfile',
