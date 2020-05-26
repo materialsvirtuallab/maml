3c95b4975f74bcb36402bbaf3eab1bbf96626987
Chi Chen <chc273@eng.ucsd.edu>
2020-05-26 14:05:42 -0700

    remove useless model

--- maml/model/__init__.py
+++ maml/model/__init__.py
@@ -1,10 +1,8 @@
 """MAML models"""
-from ._neural_network import MultiLayerPerceptron  # noqa
 from maml.base import SKLModel, KerasModel  # noqa
 
 
 __all__ = [
-    'MultiLayerPerceptron',
     'SKLModel',
     'KerasModel'
 ]
