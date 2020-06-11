a1a96b7b59dfb3c3863f7f742672b3701c9a255b
w6ye <w6ye@ucsd.edu>
2020-06-10 22:37:16 -0700

    fix test_sis

--- maml/apps/symbolic/tests/test_sis.py
+++ maml/apps/symbolic/tests/test_sis.py
@@ -70,7 +70,7 @@ class testISIS(PymatgenTest):
         selected = isis.run(self.x.values, self.y, max_p=5)
         self.assertAlmostEqual(isis.sis.gamma, 0.15)
         #     # np.testing.assert_equal(selected, [10, 11, 12,  4,  5,  6,  0,  3,  2,  8])
-        np.testing.assert_equal(selected, [10, 11, 12,  4, 5])
+        np.testing.assert_equal(selected, [10, 11, 12,  5, 0])
 
 
 if __name__ == "__main__":
