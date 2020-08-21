141f0e070facf769b4ff45935ed18759bb9f5981
Chi Chen <chc273@eng.ucsd.edu>
2020-08-21 12:20:01 -0700

    fix matminer wrapper and tests

---
+++ maml/describers/matminer_wrapper/tests/test_matminer_wrapper.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import unittest
+
+from maml.describers.matminer_wrapper import wrap_matminer_describer
+from maml.utils import to_composition
+from matminer.featurizers import composition
+from pymatgen.util.testing import PymatgenTest
+
+
+class TestWrapper(PymatgenTest):
+    @classmethod
+    def setUp(cls) -> None:
+        cls.s_li2o = PymatgenTest.get_structure("Li2O")
+        cls.s_lfp = PymatgenTest.get_structure("LiFePO4")
+
+    def test_element_prop(self):
+        ElementProperty = wrap_matminer_describer("ElementProperty", composition.ElementProperty,
+                                                  to_composition, describer_type='composition')
+        ep = ElementProperty.from_preset("magpie")
+        self.assertArrayAlmostEqual(ep.transform_one(self.s_li2o).values,
+                                    ep.transform_one("Li2O").values)
+        self.assertListEqual(ep._get_param_names(), ['data_source', 'features', 'stats'])
+
+    def test_atomic_orbitals(self):
+
+        AtomicOrbitals = wrap_matminer_describer("AtomicOrbitals", composition.AtomicOrbitals,
+                                                 to_composition, describer_type='composition')
+        ao = AtomicOrbitals()
+        for i, j in zip(ao.transform_one("LiFePO4").values, ao.transform_one(self.s_lfp).values):
+            for k, l in zip(i, j):
+                self.assertEqual(k, l)
+
+
+if __name__ == "__main__":
+    unittest.main()
