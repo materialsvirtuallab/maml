0dabc0b76e22714b37252381615e146e95716c9b
First Coder <shyuep@gmail.com>
2021-03-02 14:45:08 -0800

    Move the rfxas to just own repo.

--- maml/__init__.py
+++ maml/__init__.py
@@ -7,3 +7,5 @@ __version__ = "2021.2.27"
 from .base import BaseDataSource  # noqa
 from .base import BaseDescriber  # noqa
 from .base import BaseModel, SKLModel, KerasModel, SequentialDescriber  # noqa
+
+__import__('pkg_resources').declare_namespace(__name__)
