96707f8bc46060b332b40f6b1c33ef082dd5c43e
First Coder <shyuep@gmail.com>
2021-03-02 16:06:59 -0800

    fix linting

--- maml/__init__.py
+++ maml/__init__.py
@@ -4,4 +4,4 @@ maml - materials machine learning
 
 __version__ = "2021.3.2"
 
-__import__('pkg_resources').declare_namespace(__name__)
+__import__("pkg_resources").declare_namespace(__name__)
--- maml/apps/__init__.py
+++ maml/apps/__init__.py
@@ -2,4 +2,4 @@
 Apps utilizing maml.
 """
 
-__import__('pkg_resources').declare_namespace(__name__)
+__import__("pkg_resources").declare_namespace(__name__)
