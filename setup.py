b5f951cd8ca374e39c27eded4965d77dfebadbe3
Shyue Ping Ong <shyuep@gmail.com>
2022-05-04 09:51:32 -0700

    Fix build dependencies for maml.

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
