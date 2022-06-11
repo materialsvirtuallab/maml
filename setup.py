11428267a7df3d7437ee6c42c8f24fee05702b30
Shyue Ping Ong <sp@ong.ai>
2022-06-11 16:06:36 -0700

    Replace pylint,

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ For the ML-IAP package (`maml.pes`), please cite::
 setup(
     name="maml",
     packages=find_packages(),
-    version="2022.5.3",
+    version="2022.6.11",
     install_requires=["numpy", "scipy", "monty", "scikit-learn", "pandas", "pymatgen", "tqdm"],
     extras_requires={
         "maml.apps.symbolic._selectors_cvxpy": ["cvxpy"],
