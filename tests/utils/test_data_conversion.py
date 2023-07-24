9d711c861e688fdd5a258a21add74265d992001c
Shyue Ping Ong <sp@ong.ai>
2023-07-24 10:04:29 -0700

    Fix relative link.

--- tests/utils/test_data_conversion.py
+++ tests/utils/test_data_conversion.py
@@ -13,7 +13,7 @@ from monty.serialization import loadfn
 from maml.utils._data_conversion import convert_docs, pool_from
 
 CWD = os.getcwd()
-test_datapool = loadfn(os.path.join(os.path.dirname(__file__), "../../apps/pes/tests/datapool.json"))
+test_datapool = loadfn(os.path.join(os.path.dirname(__file__), "../apps/pes/datapool.json"))
 
 
 class PorcessingTest(unittest.TestCase):
