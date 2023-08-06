720a22ccd4f0a9503c3753b0e5c8cf005e831885
Ji Qi <qiji@jis-air-7.mynetworksettings.com>
2023-08-06 00:27:00 -0400

    use pytest fixture defined data

--- tests/sampling/test_clustering.py
+++ tests/sampling/test_clustering.py
@@ -1,37 +1,18 @@
 from __future__ import annotations
 
 import numpy as np
-import os
-import pickle
-import unittest
 
 from maml.sampling.clustering import BirchClustering
 
-PC_weighted_file_path = os.path.join(
-    os.path.dirname(__file__),
-    "data",
-    "MPF_2021_2_8_first10_test_PC_weighted.pickle",
-)
-with open(PC_weighted_file_path, "rb") as f:
-    PC_weighted = pickle.load(f)
 
-Birch_result_file_path = os.path.join(
-    os.path.dirname(__file__),
-    "data",
-    "MPF_2021_2_8_first10_Birch_results.pickle",
-)
-with open(Birch_result_file_path, "rb") as f:
-    Birch_results = pickle.load(f)
-
-
-class TestBirchClustering(unittest.TestCase):
-    def setUp(self):
+class TestBirchClustering:
+    def setup(self):
         self.Birch = BirchClustering(n=2, threshold_init=0.01)
 
-    def test_fit(self):
+    def test_fit(self, PC_weighted):
         assert self.Birch == self.Birch.fit(PC_weighted)
 
-    def test_transform(self):
+    def test_transform(self, Birch_results, PC_weighted):
         clustering_results = self.Birch.transform(PC_weighted)
         assert "labels" in clustering_results.keys()
         assert "label_centers" in clustering_results.keys()
@@ -44,7 +25,3 @@ class TestBirchClustering(unittest.TestCase):
             )
         )
         assert (clustering_results["PCAfeatures"] == Birch_results["PCAfeatures"]).all()
-
-
-if __name__ == "__main__":
-    unittest.main()
--- tests/sampling/test_pca.py
+++ tests/sampling/test_pca.py
@@ -1,42 +1,14 @@
 from __future__ import annotations
 
-import os
-import pickle
-import unittest
-
 from maml.sampling.pca import PrincipalComponentAnalysis
 
-feature_file_path = os.path.join(
-    os.path.dirname(__file__),
-    "data",
-    "M3GNet_features_MPF_2021_2_8_first10_features_test.pickle",
-)
-with open(feature_file_path, "rb") as f:
-    MPF_2021_2_8_first10_features_test = pickle.load(f)
 
-PC_weighted_file_path = os.path.join(
-    os.path.dirname(__file__),
-    "data",
-    "MPF_2021_2_8_first10_test_PC_weighted.pickle",
-)
-with open(PC_weighted_file_path, "rb") as f:
-    PC_weighted = pickle.load(f)
-
-PC_unweighted_file_path = os.path.join(
-    os.path.dirname(__file__),
-    "data",
-    "MPF_2021_2_8_first10_test_PC_unweighted.pickle",
-)
-with open(PC_unweighted_file_path, "rb") as f:
-    PC_unweighted = pickle.load(f)
-
-
-class TestPrincipalComponentAnalysis(unittest.TestCase):
-    def setUp(self):
+class TestPrincipalComponentAnalysis:
+    def setup(self):
         self.pca_weightedPC = PrincipalComponentAnalysis(weighting_PCs=True)
         self.pca_unweightedPC = PrincipalComponentAnalysis(weighting_PCs=False)
 
-    def test_fit(self):
+    def test_fit(self, MPF_2021_2_8_first10_features_test):
         assert self.pca_weightedPC == self.pca_weightedPC.fit(
             MPF_2021_2_8_first10_features_test["M3GNet_features_normalized"]
         )
@@ -44,7 +16,9 @@ class TestPrincipalComponentAnalysis(unittest.TestCase):
             MPF_2021_2_8_first10_features_test["M3GNet_features_normalized"]
         )
 
-    def test_transform(self):
+    def test_transform(
+        self, MPF_2021_2_8_first10_features_test, PC_unweighted, PC_weighted
+    ):
         assert (
             self.pca_weightedPC.fit_transform(
                 MPF_2021_2_8_first10_features_test["M3GNet_features_normalized"]
