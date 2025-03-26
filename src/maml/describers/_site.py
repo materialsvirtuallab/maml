2f30b0f40b81c872c091ca627d202e74a2e565ac
Shyue Ping Ong <shyuep@gmail.com>
2025-03-26 04:54:47 -0700

    Mypy fixes.

--- src/maml/describers/_site.py
+++ src/maml/describers/_site.py
@@ -325,7 +325,8 @@ class BPSymmetryFunctions(BaseDescriber):
             for specie in elements:
                 distances = np.array([nn_distance for _, nn_distance in temp_dict[specie]])[:, None, None]
                 g2 = np.sum(
-                    np.exp(-self.r_etas * (distances - self.r_shift) ** 2) * self._fc(distances),
+                    np.exp(-self.r_etas * (distances - self.r_shift) ** 2)
+                    * self._fc(distances),  # type:ignore[arg-type]
                     axis=0,  # type: ignore
                 )
                 site_symmfuncs.extend(g2.ravel().tolist())
